268f3d91b1cdf58dced1d8623df6b4ff577f819d
taldr27 <diegogarciaq98@gmail.com>
2022-09-14 09:18:29 -0500

    Add object for the speakers in js file

--- scripts/main.js
+++ scripts/main.js
@@ -15,4 +15,43 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   burger.addEventListener('click', toggleBar);
   document.getElementById('close-nav').addEventListener('click', toggleBar);
+
+  const speakers = [
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+    {
+      speakerImg: 'images/speaker_kiko.png',
+      speakerName: 'Kiko Loureiro',
+      speakerSchool: 'Megadeth s guitarrist',
+      speakerDescription: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    },
+  ];
 });
